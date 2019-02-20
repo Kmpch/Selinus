4f294920e347c7b9e0502b6b887791c3722b98c6
luffych <chenglulu@songxiaocai.com>
2019-02-20 21:33:28 +0800

    feat:初始化框架接口,提交

---
+++ selinus-starter/src/main/java/com/sxc/demo/starter/Application.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2012-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.sxc.demo.starter;
+
+import com.sxc.demo.sku.infrastructure.config.SkuMainConfiguration;
+import com.sxc.demo.trade.infrastructure.config.TradeMainConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.freemarker.FreeMarkerAutoConfiguration;
+import org.springframework.boot.autoconfigure.sendgrid.SendGridAutoConfiguration;
+import org.springframework.boot.autoconfigure.session.SessionAutoConfiguration;
+import org.springframework.boot.autoconfigure.websocket.WebSocketAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportResource;
+
+
+@SpringBootApplication(exclude = {WebSocketAutoConfiguration.class, SessionAutoConfiguration.class,
+        SendGridAutoConfiguration.class, FreeMarkerAutoConfiguration.class})
+
+//进入子域主配置类，各域自己spring上下文
+@Import({SkuMainConfiguration.class,TradeMainConfiguration.class})
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
